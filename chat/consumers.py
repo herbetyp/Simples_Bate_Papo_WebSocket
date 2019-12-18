81a797da663bf3931b798ef499750774470dea8e
Hp2501 <herbetyp@gmail.com>
2019-12-17 21:18:28 -0300

    finalizando o chat simples

--- chat/consumers.py
+++ chat/consumers.py
@@ -6,12 +6,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['nome_sala']
-        self.room_name = f'chat_{self.room_name}'
+        self.room_group_name = f'chat_{self.room_name}'
 
         # Entra na sala
         await self.channel_layer.group_add(
             self.room_group_name,
-            self.room_name
+            self.channel_name
         )
 
         await self.accept()
@@ -44,5 +44,5 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # Envia a mensagem para o WebSocket
         await self.send(text_data=json.dumps({
-            'mesagem': mensagem
+            'mensagem': mensagem
         }))
